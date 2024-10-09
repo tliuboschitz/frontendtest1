4ba7cd016a3502797dc45ce186fccd1f3098fe81
tliuboschitz <tliuboschitz@uade.edu.ar>
2024-10-09 17:17:39 -0300

    se crearon 3 archivos

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+const boton = document.getElementById('mi-boton');
+const parrafo = document.getElementById('mi-parrafo');
+
+boton.addEventListener('click', function() {
+    parrafo.style.backgroundColor = 'lightblue';
+});
